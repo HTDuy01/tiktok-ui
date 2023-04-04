16f3121793af77655048ad53c612e995a8df4dad
HTDuy01 <94676676+HTDuy01@users.noreply.github.com>
2023-04-04 14:07:07 +0700

    Áp dụng PropTypes vào dự án

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { forwardRef, useState } from 'react';
 import images from '~/assets/images';
 import style from './Image.module.scss';
@@ -12,5 +13,12 @@ const Image = forwardRef(({ src, alt, className, fallback: customFallback = imag
 
     return <img className={classNames(style.wrapper, className)} ref={ref} src={fallback || src} alt={alt} {...props} onError={handleError} />;
 });
+
+Image.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+};
 
 export default Image;
